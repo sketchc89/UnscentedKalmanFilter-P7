9f8ca78e76887d5433a20ff289e8020e81868a9a
Chris Sketch <sketchc89@gmail.com>
2018-05-14 21:58:24 -0400

    Tuning process noise

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -34,10 +34,10 @@ UKF::UKF() {
   P_ = MatrixXd::Identity(n_x_, n_x_);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 0.1;
+  std_a_ = 0.5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.1;
+  std_yawdd_ = 0.5;
   
   // ******************************************************************************************
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
